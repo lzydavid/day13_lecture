10ffde4db3b07ee8c587a1d4791d70b9f7ac3a49
lzydavid <nodotanolife@hotmail.com>
2023-02-08 09:52:56 +0800

    git init

---
+++ src/test/java/SSF_2022/day13/Day13ApplicationTests.java
@@ -0,0 +1,13 @@
+package SSF_2022.day13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
